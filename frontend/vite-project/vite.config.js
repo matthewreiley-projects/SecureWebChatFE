e4ef86bf5c1f48d897ee43ce3c3220926c419e50
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-08-01 16:49:45 -0700

    vite-config changes

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
+//import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // Listen on 0.0.0.0 (not just localhost)
+    port: process.env.PORT || 4173, // Railway sets PORT env variable
+    allowedHosts: ['healthcheck.railway.app'] // Allow healthcheck requests
+  }
+  // Uncomment below if you're using HTTPS certs locally
   // server: {
   //   https: {
   //     key: fs.readFileSync('./certs/server.key'),
   //     cert: fs.readFileSync('./certs/server.cert')
   //   }
   // }
-});
+})
