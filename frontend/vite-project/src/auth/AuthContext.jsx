22612f8996ae5709ff046b2986905f08012e1b23
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-08-01 17:48:00 -0700

    small tweak

--- frontend/vite-project/src/auth/AuthContext.jsx
+++ frontend/vite-project/src/auth/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 const AuthContext = createContext();
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://192.168.86.22:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://192.168.86.22:3000";
 
 
 export function AuthProvider({ children }) {
